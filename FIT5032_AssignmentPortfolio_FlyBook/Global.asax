ed0c974eb77798f98db081e0d3f46b1e562803c9
DataGen64 <bkri0003@student.monash.edu>
2019-09-16 04:55:49 +1000

    initial commit

---
+++ FIT5032_AssignmentPortfolio_FlyBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_AssignmentPortfolio_FlyBook.MvcApplication" Language="C#" %>
---
+++ FIT5032_AssignmentPortfolio_FlyBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_AssignmentPortfolio_FlyBook.Startup))]
+namespace FIT5032_AssignmentPortfolio_FlyBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
